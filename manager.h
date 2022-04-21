8514b168f3b760f3b841496a5c84fe8970e88e98
rladp127 <rladp127@naver.com>
2022-04-22 01:48:37 +0900

    Third commit for m.h

--- manager.h
+++ manager.h
@@ -17,7 +17,7 @@ void read(Product plist); // 하나의 제품 출력 함수
 int update(Product *plist); // 상품 정보 업데이트(수정)
 void search(Product *plist, int count); // 제품 이름 검색
 void listProduct(Product *plist, int count);
-int selectDataNo(Product *plist, int count);
+int selectDataNo(Product *plist, int count); // 수정할 인덱스 번호 셀렉
 
 void saveData(Product *plist, int count); // 파일 저장 
 int loadData(Product *plist); // 파일 불러오기
